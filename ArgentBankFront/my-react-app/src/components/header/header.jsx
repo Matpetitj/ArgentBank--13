b8ee8f776929b8c34bd70cb1c818fd9a6e727126
Mathis Petitjean <100531763+Matpetitj@users.noreply.github.com>
2025-04-06 17:28:46 +0200

    Create the sign in page

--- ArgentBankFront/my-react-app/src/components/header/header.jsx
+++ ArgentBankFront/my-react-app/src/components/header/header.jsx
@@ -2,7 +2,7 @@ import "./header.scoped.scss"
 
 import Logo from "../../../assets/img/argentBankLogo.png"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleUser/*FaRightFromBracket*/ } from "@fortawesome/free-solid-svg-icons";
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
 
 function Header(){
     return (
@@ -11,11 +11,24 @@ function Header(){
                 <div className="logoDiv">
                     <img src={Logo} alt="Logo de ArgentBank" href="/"></img>
                 </div>
-                <div className="HeaderProfil">
+                <div className="headerProfil">
+                    {/* <div className="headerConnected">
+                        <a className="headerSignIn" href="/user">
+                            <FontAwesomeIcon className="signInIcon"
+                            icon={faCircleUser}/>
+                            Tony
+                        </a>
+                        <a className="headerSignIn" href="/">
+                            <FontAwesomeIcon className="signInIcon"
+                            icon={faRightFromBracket}/>
+                            Sign Out
+                        </a>
+                    </div> */}
                     <a className="headerSignIn" href="/login">
-                        <FontAwesomeIcon className="SignInIcon" icon={faCircleUser}/>
+                        <FontAwesomeIcon className="signInIcon"
+                        icon={faCircleUser}/>
+                        Sign In
                     </a>
-                    Sign In
                 </div>
             </nav>
         </>
--- ArgentBankFront/my-react-app/src/pages/SignIn/SignIn.jsx
+++ ArgentBankFront/my-react-app/src/pages/SignIn/SignIn.jsx
@@ -1,5 +1,54 @@
+import "./signin.scoped.scss";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+
 function SignIn(){
 
+    const handleSubmit = async (event) =>{
+        event.preventDefault();
+    }
+
+    return (
+        <>
+            <main className="signIn">
+                <section className="signInFormContainer">
+                    <FontAwesomeIcon className="signInIcon" icon={faCircleUser}/>
+                    <h1 className="signInFormTitle">Sign In</h1>
+                    <form className="signInForm" onSubmit={handleSubmit}>
+                        <div className="signInInputContainer">
+                            <label hmtlfor="username">Username</label>
+                            <input className="signInInput"
+                            type="text"
+                            id="username"
+                            // value={userName}
+                            // onChange={}
+                            />
+                        </div>
+                        <div className="signInInputContainer">
+                            <label hmtlfor="password">Password</label>
+                            <input className="signInInput"
+                            type="password"
+                            id="password"
+                            // value={userPassword}
+                            // onChange={}
+                            />
+                        </div>
+                        <div className="signInInputRemember">
+                            <input className="signInInput"
+                            type="checkbox"
+                            id="remember-me"
+                            // value={userName}
+                            // onChange={}
+                            />
+                            <label hmtlfor="remember-me">Remember me</label>
+                        </div>
+                        <button className="signInButton">Sign In</button>
+                    </form>
+                </section>
+            </main>
+        </>
+    )
 }
 
 export default SignIn;
